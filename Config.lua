b1b38d7019d840e8b0b08d9d8118cf81fee1e133
rockrocks8 <83791138+rockrocks8@users.noreply.github.com>
2022-07-03 23:30:50 +0100

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -3,357 +3,104 @@ Config = {}
 Config.UseBlip = true                      -- [true to enable the blip | false disables the blip]
 
 Config.Location = {
-    Coords = vector4(-279.14, 2206.02, 129.85 - 1.00, 67.35),
-    ModelName = "a_m_y_epsilon_02",
-    ModelHash = 0xAA82FF9B,
-    SetBlipSprite = 303,
+    Coords = vector4(-511.88, -1747.67, 18.23, 236.77),
+    ModelName = "s_m_m_gaffer_01",
+    ModelHash = 0xA956BD9E,
+    SetBlipSprite = 467,
     SetBlipDisplay = 6,
     SetBlipScale = 0.85,
-    SetBlipColour = 40,
-    BlipName = "BlackMarket Dealer"
+    SetBlipColour = 2,
+    BlipName = "Material Dealer"
 }
 
 Config.Icons = {
     EyeIcon = "fa-solid fa-face-smile-horns",
-    Header = "fa-solid fa-person-rifle",
-    Pistol = "fa-solid fa-gun",
-    SubMachineGuns = "fa-solid fa-gun",
-    Shotguns = "fa-solid fa-gun",
-    AssaultWeapons = "fa-solid fa-gun",
-    Miscellanceous = "fa-solid fa-icons"
+    Header = "fa-solid fa-paste",
+    Rare = "fa-solid fa-recycle",
+    Miscellanceous = "fa-solid fa-recycle"
 }
 
 Config.Text = {
-    TargetLabel = "BlackMarket Seller",
-    PedHeader = "BlackMarket Dealer",
-    Pistols = "Pistols",
-    SubMachineGuns = "SubMachine Guns",
-    Shotguns = "Shotguns",
-    AssaultWeapons = "Assault Rifles",
+    TargetLabel = "Materials Seller",
+    PedHeader = "Materials Dealer",
+    Rare = "Rare",
     Miscellanceous = "Miscellanceous",
-
 }
 
-Config.PistolShop = {
-    label = "Black Market Pistols",
+Config.RareShop = {
+    label = "Rare Materials",
     slots = 10,
     items = {
         [1] = {
-            name = "weapon_pistol",
-            price = 3000,
-            amount = 100,
+            name = "rubber",
+            price = 55,
+            amount = 200,
             info = {},
             type = "item",
             slot = 1,
-        },
-        [2] = {
-            name = "weapon_pistol_mk2",
-            price = 1600,
-            amount = 100,
-            info = {},
-            type = "item",
-            slot = 2,
-        },
-        [3] = {
-            name = "weapon_heavypistol",
-            price = 3000,
-            amount = 100,
-            info = {},
-            type = "item",
-            slot = 3,
-        },
-        [4] = {
-            name = "weapon_marksmanpistol",
-            price = 5000,
-            amount = 100,
-            info = {},
-            type = "item",
-            slot = 4,
-        },
-        [5] = {
-            name = "weapon_snspistol_mk2",
-            price = 5000,
-            amount = 100,
-            info = {},
-            type = "item",
-            slot = 5,
-        },
-        [6] = {
-            name = "weapon_navyrevolver",
-            price = 5000,
-            amount = 100,
-            info = {},
-            type = "item",
-            slot = 6,
-        }
-    }
-}
-
-Config.SubMachineGunShop = {
-    label = "Black Market SubMachine Guns",
-    slots = 10,
-    items = {
-        [1] = {
-            name = "weapon_microsmg",
-            price = 5000,
-            amount = 100,
-            info = {},
-            type = "item",
-            slot = 1,
-        },
-        [2] = {
-            name = "weapon_smg_mk2",
-            price = 6000,
-            amount = 100,
-            info = {},
-            type = "item",
-            slot = 2,
-        },
-        [3] = {
-            name = "weapon_assaultsmg",
-            price = 6500,
-            amount = 100,
-            info = {},
-            type = "item",
-            slot = 3,
-        },
-        [4] = {
-            name = "weapon_combatpdw",
-            price = 6000,
-            amount = 100,
-            info = {},
-            type = "item",
-            slot = 4,
-        },
-        [5] = {
-            name = "weapon_machinepistol",
-            price = 5000,
-            amount = 100,
-            info = {},
-            type = "item",
-            slot = 5,
-        },
-        [6] = {
-            name = "weapon_minismg",
-            price = 5000,
-            amount = 100,
-            info = {},
-            type = "item",
-            slot = 6,
-        }
-    }
-}
-
-Config.ShotGunShop = {
-    label = "Black Market ShotGuns",
-    slots = 10,
-    items = {
-        [1] = {
-            name = "weapon_pumpshotgun",
-            price = 12000,
-            amount = 100,
-            info = {},
-            type = "item",
-            slot = 1,
-        },
-        [2] = {
-            name = "weapon_sawnoffshotgun",
-            price = 8000,
-            amount = 100,
-            info = {},
-            type = "item",
-            slot = 2,
-        },
-        [3] = {
-            name = "weapon_bullpupshotgun",
-            price = 12500,
-            amount = 100,
-            info = {},
-            type = "item",
-            slot = 3,
-        },
-        [4] = {
-            name = "weapon_dbshotgun",
-            price = 13000,
-            amount = 100,
-            info = {},
-            type = "item",
-            slot = 4,
-        },
-        [5] = {
-            name = "weapon_heavyshotgun",
-            price = 15000,
-            amount = 100,
-            info = {},
-            type = "item",
-            slot = 5,
-        },
-        [6] = {
-            name = "weapon_pumpshotgun_mk2",
-            price = 10000,
-            amount = 100,
-            info = {},
-            type = "item",
-            slot = 6,
-        }
-    }
-}
-
-Config.AssaultWeaponsShop = {
-    label = "Black Market Assault Weapon Shop",
-    slots = 10,
-    items = {
-        [1] = {
-            name = "weapon_assaultrifle",
-            price = 26000,
-            amount = 100,
-            info = {},
-            type = "item",
-            slot = 1,
-        },
-        [2] = {
-            name = "weapon_assaultrifle_mk2",
-            price = 30000,
-            amount = 100,
-            info = {},
-            type = "item",
-            slot = 2,
-        },
-        [3] = {
-            name = "weapon_carbinerifle_mk2",
-            price = 42000,
-            amount = 100,
-            info = {},
-            type = "item",
-            slot = 3,
-        },
-        [4] = {
-            name = "weapon_compactrifle",
-            price = 28500,
-            amount = 100,
-            info = {},
-            type = "item",
-            slot = 4,
-        },
-        [5] = {
-            name = "weapon_advancedrifle",
-            price = 32000,
-            amount = 100,
-            info = {},
-            type = "item",
-            slot = 5,
-        },
-        [6] = {
-            name = "weapon_bullpuprifle_mk2",
-            price = 36000,
-            amount = 100,
-            info = {},
-            type = "item",
-            slot = 6,
         }
     }
 }
 
 Config.MiscellanceousShop = {
-    label = "Black Market Miscellanceous Shop",
+    label = "Miscellanceous Materials",
     slots = 10,
     items = {
         [1] = {
-            name = "armor",
-            price = 10000,
-            amount = 100,
+            name = "glass",
+            price = 17,
+            amount = 200,
             info = {},
             type = "item",
             slot = 1,
         },
         [2] = {
-            name = "heavyarmor",
-            price = 15000,
-            amount = 100,
+            name = "steel",
+            price = 19,
+            amount = 200,
             info = {},
             type = "item",
             slot = 2,
         },
         [3] = {
-            name = "labkey",
-            price = 30000,
-            amount = 100,
+            name = "iron",
+            price = 21,
+            amount = 200,
             info = {},
             type = "item",
             slot = 3,
         },
         [4] = {
-            name = "trojan_usb",
-            price = 75000,
-            amount = 100,
+            name = "aluminum",
+            price = 17,
+            amount = 200,
             info = {},
             type = "item",
             slot = 4,
         },
         [5] = {
-            name = "thermite",
-            price = 200000,
-            amount = 100,
+            name = "copper",
+            price = 20,
+            amount = 200,
             info = {},
             type = "item",
             slot = 5,
         },
         [6] = {
-            name = "drill",
-            price = 175000,
-            amount = 100,
+            name = "metalscrap",
+            price = 19,
+            amount = 200,
             info = {},
             type = "item",
             slot = 6,
         },
         [7] = {
-            name = "advancedlockpick",
-            price = 5000,
-            amount = 100,
+            name = "plastic",
+            price = 16,
+            amount = 200,
             info = {},
             type = "item",
             slot = 7,
         },
-        [8] = {
-            name = "gatecrack",
-            price = 950000,
-            amount = 100,
-            info = {},
-            type = "item",
-            slot = 8,
-        },
-        [9] = {
-            name = "pistol_ammo",
-            price = 100,
-            amount = 100,
-            info = {},
-            type = "item",
-            slot = 9,
-        },
-        [10] = {
-            name = "rifle_ammo",
-            price = 1500,
-            amount = 100,
-            info = {},
-            type = "item",
-            slot = 10,
-        },
-        [11] = {
-            name = "smg_ammo",
-            price = 1000,
-            amount = 100,
-            info = {},
-            type = "item",
-            slot = 11,
-        },
-        [12] = {
-            name = "shotgun_ammo",
-            price = 2000,
-            amount = 100,
-            info = {},
-            type = "item",
-            slot = 12,
-        },
     }
 }
-
